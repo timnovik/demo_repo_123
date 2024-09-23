41bf8f926d404768218f13da217fcfb6b9974d24
Timofey Novikov <t13novik@gmail.com>
2024-09-23 20:00:46 +0300

    small changes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 logs = open("logs/log.txt", "a")
 for i in range(int(input())):
     name = input()
-    print(f"hello {name}!")
+    print(f"Hello, {name}!")
     print(name, file=logs)
 logs.close()
